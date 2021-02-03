86ea228449d69c9deb3dcd6ba5744d1a285aef60
kubotan <70894341+kubotan@users.noreply.github.com>
2021-02-04 02:14:27 +0900

    Create mjpg_streamer.sh

---
+++ mjpg_streamer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+declare -A devices=(
+  ['/dev/video0']=8000
+  ['/dev/video1']=8001
+)
+
+is_reprobe=0
+for device in "${!devices[@]}"
+do
+  if [ `ls -l $device | /usr/bin/wc -l` -ne 1 ]
+  then
+    echo 'No found:'$device
+    is_reprobe=1
+  fi
+done
+
+if [ $is_reprobe -eq 1 ]
+then
+  echo 'Start reprobe...'
+  /usr/bin/pkill mjpg_streamer
+  /sbin/rmmod uvcvideo
+  /sbin/modprobe uvcvideo
+fi
+
+for device in "${!devices[@]}"
+do
+  if [ `ps aux | grep 'mjpg_streamer' | grep $device | /usr/bin/wc -l` -ne 1 ]
+  then
+    echo 'Restart port:'${devices[$device]}
+    /usr/local/bin/mjpg_streamer -b -i 'input_uvc.so -f 30 -r 1920x1080 -d '$device' -n -q 100 -dv_timings' -o 'output_http.so -p '${devices[$device]}
+  fi
+done
